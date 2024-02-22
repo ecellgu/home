ab51e1669c4e72831840c1eac582673fc6c294ec
Nishant4coding <srivastava4nishant@gmail.com>
2024-02-22 22:44:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Avirat from './Pages/Avirat';
 function App() {
